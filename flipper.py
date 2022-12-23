a6ae7e915caee3eaeb417b5c6b82eb129a446009
jslay88 <justin.slay@gmail.com>
2022-12-23 10:53:44 -0700

    Optional Webhook, exit status, Comments

--- flipper.py
+++ flipper.py
@@ -1,3 +1,5 @@
+# Check for stock availability of Flipper Zero
+# with optional Discord Webhook Notification
 import json
 import os
 
@@ -5,43 +7,44 @@ import requests
 from bs4 import BeautifulSoup
 
 
-WEBHOOK_URL = os.getenv('WEBHOOK_URL')
-if not WEBHOOK_URL:
-    raise ValueError('No WEBHOOK_URL provided')
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
 
 def send_notification(message: str):
-    requests.post(
-        WEBHOOK_URL,
-        json={'content': message}
-    )
+    # Discord Channel Webhook
+    if not WEBHOOK_URL:
+        return
+    requests.post(WEBHOOK_URL, json={"content": message})
 
 
 def check_stock():
-    resp = requests.get('https://shop.flipperzero.one')
+    resp = requests.get("https://shop.flipperzero.one")
     if resp.status_code != 200:
-        send_notification(f'Status Code Error: {resp.status_code}')
+        send_notification(f"Status Code Error: {resp.status_code}")
         return
-    soup = BeautifulSoup(resp.content.decode('utf-8'), 'html.parser')
-    results = soup.find_all('script', id=lambda x: x and x.startswith('ProductJson-'))
+    soup = BeautifulSoup(resp.content.decode("utf-8"), "html.parser")
+    results = soup.find_all(
+        "script", id=lambda x: x and x.startswith("ProductJson-")
+    )
     if not results:
-        send_notification('Unable to find ProductJson')
+        send_notification("Unable to find ProductJson")
         return
     data = json.loads(results[0].text)
-    if 'available' not in data:
-        send_notification('available key missing from ProductJson')
+    if "available" not in data:
+        send_notification("available key missing from ProductJson")
         return
-    if data.get('available'):
-        send_notification('Product Available!\n\nhttps://shop.flipperzero.one')
-        print('Product Available')
+    if data.get("available"):
+        send_notification("Product Available!\n\nhttps://shop.flipperzero.one")
+        print("Product Available")
         return True
-    print('Product Unavailable')
+    print("Product Unavailable")
     return False
 
 
 def main():
-    check_stock()
+    if not check_stock():
+        exit(1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
